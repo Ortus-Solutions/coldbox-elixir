3411d0f919061ce1a98d0602e13f24a5bb0439c0
Eric Peterson <eric@elpete.com>
2018-01-22 11:47:13 -0700

    Set the public path for ExtractTextPlugin on CSS

--- src/utils/cssLoaders.js
+++ src/utils/cssLoaders.js
@@ -47,7 +47,8 @@ module.exports = function cssLoaders(options = {}) {
         if (options.extract) {
             return ExtractTextPlugin.extract({
                 use: loaders,
-                fallback: "vue-style-loader"
+                fallback: "vue-style-loader",
+                publicPath: "/"
             });
         } else {
             return ["vue-style-loader"].concat(loaders);
