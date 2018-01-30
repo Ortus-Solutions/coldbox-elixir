6f54cb6ccad1819ab06f777d02d8d81d5a891d8e
Eric Peterson <eric@elpete.com>
2018-01-29 22:39:25 -0700

    Clean up copied files before copying

--- src/config/ingredients/copy.js
+++ src/config/ingredients/copy.js
@@ -1,7 +1,15 @@
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
 module.exports = function(from, to) {
     this.dependencies(["copy-webpack-plugin"]);
     const CopyWebpackPlugin = require("copy-webpack-plugin");
     return this.mergeConfig({
-        plugins: [new CopyWebpackPlugin([{ from, to }])]
+        plugins: [
+            new CopyWebpackPlugin([{ from, to }]),
+            new CleanWebpackPlugin([to], {
+                root: global.elixir.rootPath,
+                verbose: false
+            })
+        ]
     });
 };
