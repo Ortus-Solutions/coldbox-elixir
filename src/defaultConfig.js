d09187548c0f360c27a7c65d47f60cc1ac915eb6
Jon Clausen <jon_clausen@silowebworks.com>
2022-02-10 16:09:06 -0500

    Vue 3 updates

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -44,8 +44,7 @@ module.exports = () => ({
                                 }
                             }
                         ]
-                    ],
-                    plugins: [ "@babel/plugin-proposal-object-rest-spread"]
+                    ]
                 })
             },
             {
