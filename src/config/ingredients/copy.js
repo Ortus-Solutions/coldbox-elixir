fe72d8c4f77ae9d5296f82698c32597a5c2c6641
Jonathan Clausen <jclausen@ortussolutions.com>
2023-02-22 17:02:08 -0500

    Issue #47 - Allow mix.copy paths to be absolute

--- src/config/ingredients/copy.js
+++ src/config/ingredients/copy.js
@@ -1,13 +1,29 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
+const path = require("path");
+
 module.exports = function(from, to) {
     if (this.dependencies(["copy-webpack-plugin@10.2.4"])) {
         return;
     }
     const CopyWebpackPlugin = require("copy-webpack-plugin");
+    let fromPath = from.substring( 0, 1 ) == "/"
+                    ? from
+                    : path.resolve(
+                        global.elixir.rootPath,
+                        this.prefix,
+                        from
+                    );
+    let toPath = to.substring( 0, 1 ) == "/"
+                    ? to
+                    : path.resolve(
+                        global.elixir.rootPath,
+                        this.prefix,
+                        to
+                    );;
     return this.mergeConfig({
         plugins: [
-            new CopyWebpackPlugin({ patterns: [ { from: from, to: to } ] }),
+            new CopyWebpackPlugin({ patterns: [ { from: fromPath, to: toPath } ] }),
             new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [to] })
         ]
     });
